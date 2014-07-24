294a7eac1ff569c431cfbc64fc9d8bb5593b5125
gomenasaai <contramundee@gmail.com>
2014-07-24 20:32:25 +0100

    added helper methods for genres, media in work model with rspec tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140724182127) do
+ActiveRecord::Schema.define(version: 20140724185044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,10 @@ ActiveRecord::Schema.define(version: 20140724182127) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "caption"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "works", ["user_id"], name: "index_works_on_user_id", using: :btree
